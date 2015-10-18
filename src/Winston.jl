00790e950d7ace749f2aee7f4280c7e0816a5a3d
hustf <frohust@gmail.com>
2015-10-19 00:18:28 +0200

    modified:        Issue 246   Winston.jl:97 } (   ->   }( plot.jl:572      FloatingPoint ~> AbstractFloat renderer.jl:63   color_to_rgb(s::AbstractString) =parse(Colorant,s)

--- src/Winston.jl
+++ src/Winston.jl
@@ -99,7 +99,7 @@ import Base: copy,
 export get_context, device_to_data, data_to_device
 
 if VERSION < v"0.3-"
-    typealias AbstractVecOrMat{T} (@compat Union{AbstractVector{T}, AbstractMatrix{T}})
+    typealias AbstractVecOrMat{T}(@compat Union{AbstractVector{T}, AbstractMatrix{T}})
     extrema(x) = (minimum(x),maximum(x))
     Base.push!(x, a, b) = (push!(x, a); push!(x, b))
 elseif VERSION < v"0.4-"
